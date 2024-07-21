14bdd6cf7f569be3083117917d21833cd0438d98
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-07-21 22:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+function minCost(arr) {
+    // Create a priority queue and add all the rope lengths
+    let queue = new PriorityQueue();
+    for (let i = 0; i < arr.length; i++) {
+        queue.add(arr[i]);
+    }
 
-module.exports=mincost;
+    // Initialize the total cost to 0
+    let totalCost = 0;
+
+    // While there's more than one rope left...
+    while (queue.size() > 1) {
+        // Take the two shortest ropes from the queue
+        let cost = queue.poll() + queue.poll();
+
+        // Add the cost to the total cost
+        totalCost += cost;
+
+        // Put the resulting rope back into the queue
+        queue.add(cost);
+    }
+
+    // The total cost is now the minimum cost to connect all ropes
+    return totalCost;
+}
